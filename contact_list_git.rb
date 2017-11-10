4e2b53a4f2cb2edb26945e3c73621349a58b52ba
Pete <palberico@yahoo.com>
2017-11-10 12:24:51 -0700

    contact loop not working

--- contact_list_git.rb
+++ contact_list_git.rb
@@ -7,10 +7,13 @@ puts "1) View all contacts"
 puts "2) Add a Contact"
 puts '3) Exit'
 
+contacts ['jake', 'dave', 'pete']
 user_input = gets.to_i
+
 case user_input
   when 1
-      puts 'show all contacts'
+    contacts.each.do |contacts|
+      puts 'puts "Contact; #contact"
   when 2
     puts 'add new contact'
   when 3
