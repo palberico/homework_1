4fe47d6f203fe65507c0f9c3b25531cacb28bed8
Pete <palberico@yahoo.com>
2017-11-10 12:10:30 -0700

    user choice and menu working

--- contact_list_git.rb
+++ contact_list_git.rb
@@ -6,3 +6,17 @@ puts "--Contact List--"
 puts "1) View all contacts"
 puts "2) Add a Contact"
 puts '3) Exit'
+
+user_input = gets.to_i
+case user_input
+  when 1
+      puts 'show all contacts'
+  when 2
+    puts 'add new contact'
+  when 3
+    puts 'adios!'
+    exit
+  else
+    puts 'Bad Input'
+
+end
